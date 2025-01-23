1dcf573e30077e139120c56f40d54be723addc2e
Perrel-Brown <amogus69069@gmail.com>
2025-01-23 02:02:25 +0000

    batukahn

---
+++ Ambasing.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+// Function to get the computer's move
+char getComputerMove() {
+    int move = rand() % 3; // Generate a random number between 0 and 2
+    if (move == 0) return 'r'; // Rock
+    else if (move == 1) return 'p'; // Paper
+    return 's'; // Scissors
+}
+
+// Function to get the player's move
+char getPlayerMove() {
+    char move;
+    cout << "Enter your move (r for rock, p for paper, s for scissors): ";
+    cin >> move;
+    return move;
+}
+
+// Function to determine the winner
+void determineWinner(char player, char computer) {
+    if (player == computer) {
+        cout << "It's a tie!" << endl;
+    } else if ((player == 'r' && computer == 's') || 
+               (player == 'p' && computer == 'r') || 
+               (player == 's' && computer == 'p')) {
+        cout << "You win!" << endl;
+    } else {
+        cout << "Computer wins!" << endl;
+    }
+}
+
+int main() {
+    srand(static_cast<unsigned int>(time(0))); // Seed for random number generation
+    char playerMove = getPlayerMove(); // Get player's move
+    char computerMove = getComputerMove(); // Get computer's move
+
+    cout << "You chose: " << playerMove << endl;
+    cout << "Computer chose: " << computerMove << endl;
+
+    determineWinner(playerMove, computerMove); // Determine the winner
+
+    return 0;
+}
